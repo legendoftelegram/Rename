f17c2a5ebd81ba7198ad652bd84f7bcd8b3a8c6b
king legend <49789123+anandus0070@users.noreply.github.com>
2020-05-26 06:05:11 -0700

    Create thumrips.py

---
+++ plugins/thumrips.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) king legend
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+import random, re
+
+from sample_config import Config
+
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+from pyrogram import Client,Filters, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.document)
+async def thumrip(bot, message):
+    await thumb.download(thumbse) 
+    await bot.send_photo(
+      document=thumbse,
+      chat_id=message.chat.id
+    )

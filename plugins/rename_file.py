4985c8b8c652fab43e4f94b3693c03f39a9bbd9b
king legend <49789123+anandus0070@users.noreply.github.com>
2020-06-06 21:12:17 -0700

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -59,7 +59,7 @@ async def rename_doc(bot, update):
 		message_ids=update.message_id)
         c_time = time.time()
         the_real_download_location = await bot.download_media(
-            message=update.reply_to_message,
+            message=update.document,
             file_name=download_location,
             progress=progress_for_pyrogram,
             progress_args=(

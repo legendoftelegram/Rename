27a4dc4122a4f1f712bb8f9e89b9dfe85f096a6a
king legend <49789123+anandus0070@users.noreply.github.com>
2020-06-14 00:37:53 -0700

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -81,7 +81,7 @@ async def rename_doc(bot, update):
             logger.info(the_real_download_location)
             thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
             if not os.path.exists(thumb_image_path):
-		mes = await thumb(update.from_user.id)
+                mes = await thumb(update.from_user.id)
                 if mes != None:
                     m = await bot.get_messages(update.chat.id, mes.msg_id)
                     await m.download(file_name=thumb_image_path)

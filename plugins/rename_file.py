4b412bfd69eac235cc0711c3c56b087f1f3be0c5
king legend <49789123+anandus0070@users.noreply.github.com>
2020-06-14 01:22:09 -0700

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -69,7 +69,11 @@ async def rename_doc(bot, update):
         )
         await bot.delete_messages(chat_id=update.chat.id, message_ids=a.message_id)
         await bot.send_message(chat_id=update.chat.id, text="enter file name", reply_markup=ForceReply)
-	await bot.send_message(chat_id=update.chat.id, text="wait a bit", reply_to_message_id=update.message_id)	
+	await bot.send_message(
+		chat_id=update.chat.id, 
+		text="wait a bit", 
+		reply_to_message_id=update.message_id
+	)	
         if the_real_download_location is not None:
 	    file_name = update.message.reply_to_message.message_id
             new_file_name = download_location + file_name

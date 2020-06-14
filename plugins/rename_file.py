010f6632e187fa3136a9ba9889f2a4691036ce71
king legend <49789123+anandus0070@users.noreply.github.com>
2020-06-14 01:05:43 -0700

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -75,7 +75,7 @@ async def rename_doc(bot, update):
                 chat_id=update.chat.id,
                 text="enter file name",
                 reply_markup=ForceReply(selective=True)
-            )
+         )
 	 await bot.send_message(
                 chat_id=update.chat.id,
                 text="wait a bit",

07ee43fa35e5a6c7cee8315e7f6cf57c8abbe98c
king legend <49789123+anandus0070@users.noreply.github.com>
2020-06-20 23:05:18 -0700

    Update fileinfo.py

--- plugins/fileinfo.py
+++ plugins/fileinfo.py
@@ -52,7 +52,7 @@ async def meta_link(bot, update):
             chat_id=update.chat.id,
             message_id=a.message_id
         )
-        input_file =open("download_file_name")
+        input_file =open("DOWNLOAD_LOCATION/download_file_name")
         exe = "hachoir-metadata"
         process = subprocess.Popen([exe, input_file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         for output in process.stdout:

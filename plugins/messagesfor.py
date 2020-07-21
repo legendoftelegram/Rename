e7ded8c8b59335f93bd3692e2f32387ec8b5a354
king legend <49789123+anandus0070@users.noreply.github.com>
2020-07-21 05:17:55 -0700

    Update messagesfor.py

--- plugins/messagesfor.py
+++ plugins/messagesfor.py
@@ -25,7 +25,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 @pyrogram.Client.on_message(pyrogram.Filters.document)
 async def getchat(bot, update):
-    out = await bot.get_messages(chat_id=update.chat.id, message_id=update.message_id)
+    out = await bot.get_messages(chat_id=update.chat.id, message_ids=update.message_ids)
     print(out) 
     await bot.send_message(
       text=Translation.USER_TXT.format(out.id, out.type, out.is_verified, out.is_restricted, out.is_scam, out.is_support, out.first_name, out.last_name, out.username),

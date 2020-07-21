b3edc8459965cea22c51b535059bc8e90abaa5c0
king legend <49789123+anandus0070@users.noreply.github.com>
2020-07-21 03:50:12 -0700

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -14,56 +14,47 @@ import time
 
 from sample_config import Config
 
-
 # the Strings used for this "thing"
 from translation import Translation
 
 userids = []
 
-async def progress_for_pyrogram(client, current, total, ud_type, message_id, chat_id, start, update, bot):
+async def progress_for_pyrogram(
+    current,
+    total,
+    ud_type,
+    message,
+    start
+):
     now = time.time()
     diff = now - start
     if round(diff % 3.00) > 2.999 or current == total:
         # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
         speed = current / diff
-        #elapsed_time = round(diff) * 1000
+        elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
-        time_to_completion = await time_formatter(milliseconds=time_to_completion)
-        #estimated_total_time = elapsed_time + time_to_completion
-        #elapsed_time = await time_formatter(milliseconds=elapsed_time)
-        #estimated_total_time = await time_formatter(milliseconds=estimated_total_time)
+        estimated_total_time = elapsed_time + time_to_completion
 
-        progress = "<code>[{0}{1}]| {2}%</code>\n\n".format(
-            ''.join(["◼️" for i in range(math.floor(percentage / 5))]),
-            ''.join(["◻️" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        tmp = progress + "<b>{}</b> <b>of</b> <b>{}</b>\n<b>Speed:</b> <b>{}/s</b>\n<b>Remaining:</b> <b>{}</b> /Cancel".format(
+        tmp = "<b>{}</b> <b>of</b> <b>{}</b>\n<b>Speed:</b> <b>{}/s</b>\n<b>Remaining:</b> <b>{}</b> @T".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
-            time_to_completion if time_to_completion != '' else "0 s"
+            # elapsed_time if elapsed_time != '' else "0 s",
+            estimated_total_time if estimated_total_time != '' else "0 s"
         )
-        if update.chat.id in userids:
-            userids.remove(update.chat.id) 
-            await client.send_message(
-                chat_id=chat_id,
-                text="cancelled"
+        try:
+            await message.edit(
+                text="{}\n {}".format(
+                    ud_type,
+                    tmp
+                )
             )
-            await client.stop_transmission()
-
-    try:
-        await client.edit_message_text(
-            chat_id,
-            message_id,
-            text="{}\n {}".format(
-                ud_type,
-                tmp
-            )
-        )
-    except:
-        pass
+        except:
+            pass
 
 
 def humanbytes(size):
@@ -80,14 +71,14 @@ def humanbytes(size):
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
 
-async def time_formatter(milliseconds: int) -> str:
+def TimeFormatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     tmp = ((str(days) + "d, ") if days else "") + \
-          ((str(hours) + "h, ") if hours else "") + \
-          ((str(minutes) + "m, ") if minutes else "") + \
-          ((str(seconds) + "s, ") if seconds else "") + \
-          ((str(milliseconds) + "ms, ") if milliseconds else "")
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
     return tmp[:-2]
